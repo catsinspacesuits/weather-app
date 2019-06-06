4fd6b89f6a12434bdb24752a235c8259db0893be
catsinspacesuits <simunro@hotmail.co.uk>
2019-06-07 01:03:45 +0200

    add error handling

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,10 +1,9 @@
 class ForecastsController < ApplicationController
-  
   def current_weather
     @token = Rails.application.credentials.openweather_key
     @city = params[:q]
-    if @city == nil
-      @forecast = ""
+    if @city.nil?
+      @forecast = {}
     else
       @forecast = OpenWeatherApi.new(@city, @token).my_location_forecast
     end
