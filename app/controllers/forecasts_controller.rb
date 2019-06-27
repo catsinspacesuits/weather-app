f080e945a33a5ab58ee4ad000a7a46db553c8271
catsinspacesuits <simunro@hotmail.co.uk>
2019-06-27 19:51:03 +0200

    add styles and background image

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -10,5 +10,9 @@ class ForecastsController < ApplicationController
     end
     @temperature = @forecast.dig('main', 'temp').to_i - 273
     @weather_code = @forecast.dig('weather', 0, 'id').to_i
+    @description = @forecast.dig('weather', 0, 'description')
+    @short_description = @forecast.dig('weather', 0, 'main')
+    @wind = @forecast.dig('weather', 0, 'wind', 'speed')
+    @humidity = @forecast.dig('main', 'humidity')
   end
 end
