f9b00aaf28e7f98bf4a9320caabff88d8e98bff2
catsinspacesuits <simunro@hotmail.co.uk>
2019-07-13 19:36:06 +0200

    remove require_master_key to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.require_master_key = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
